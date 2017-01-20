de3176de0f4c0ca16879a517b2b585827a096218
Artur Ovasafyan <arthurhovasap@gmail.com>
2017-01-20 15:42:19 +0300

    Empty DB is ready and  db folder

---
+++ db/empty.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Янв 20 2017 г., 13:30
+-- Версия сервера: 10.1.13-MariaDB
+-- Версия PHP: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `trade100`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_company`
+--
+
+CREATE TABLE `tbl_company` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_post`
+--
+
+CREATE TABLE `tbl_post` (
+  `id` int(3) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_role`
+--
+
+CREATE TABLE `tbl_role` (
+  `id` int(2) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text,
+  `score` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_status`
+--
+
+CREATE TABLE `tbl_status` (
+  `id` int(2) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `role_id` int(2) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `post_id` int(3) NOT NULL,
+  `tel_home` varchar(20) NOT NULL,
+  `tel_work` varchar(20) NOT NULL,
+  `email_home` varchar(100) NOT NULL,
+  `email_work` varchar(100) NOT NULL,
+  `status_id` int(2) NOT NULL,
+  `created_dt` int(11) NOT NULL,
+  `updated_dt` int(11) DEFAULT NULL,
+  `by_user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `tbl_company`
+--
+ALTER TABLE `tbl_company`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `tbl_post`
+--
+ALTER TABLE `tbl_post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `tbl_role`
+--
+ALTER TABLE `tbl_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`),
+  ADD KEY `company_id` (`company_id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `by_user_id` (`by_user_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `tbl_company`
+--
+ALTER TABLE `tbl_company`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `tbl_post`
+--
+ALTER TABLE `tbl_post`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `tbl_role`
+--
+ALTER TABLE `tbl_role`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD CONSTRAINT `user_by_user_id` FOREIGN KEY (`by_user_id`) REFERENCES `tbl_user` (`id`),
+  ADD CONSTRAINT `user_company_id` FOREIGN KEY (`company_id`) REFERENCES `tbl_company` (`id`),
+  ADD CONSTRAINT `user_post_id` FOREIGN KEY (`post_id`) REFERENCES `tbl_post` (`id`),
+  ADD CONSTRAINT `user_role_id` FOREIGN KEY (`role_id`) REFERENCES `tbl_role` (`id`) ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
