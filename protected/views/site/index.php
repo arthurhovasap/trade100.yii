48f4f5d45854dbf2644c7c0a514ec3fa68700116
Artur Ovasafyan <arthurhovasap@gmail.com>
2017-01-21 16:49:47 +0300

    Filemap

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,8 +5,13 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <h1>Велком <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-Краткая Карта файлов
+<h2>Ссылки</h2>
+<ol>
+    <li>GIT: https://github.com/arthurhovasap/trade100.yii</li>
+    <li>Demo: http://tradeyii.isystems.am/</li>
+    <li>ZIP: http://tradeyii.isystems.am/trade100.dev.zip</li>
+</ol>
+<h2>Краткая Карта файлов</h2>
 <ul>
     <li>/protected/config/* - конфиги сайта</li>
     <li>/protected/helper/* - вспомогательные классы</li>
@@ -18,3 +23,10 @@ $this->pageTitle=Yii::app()->name;
     <li>/protected/modules/* - Модули сайта</li>
     <li>/protected/modules/admin - Админ модуль</li>
 </ul>
+
+<h2><i>Примичение *</i></h2>
+<dl>
+    <dt>Данные авторизации</dt>
+        <dd>Логин: admin</dd>
+        <dd>Пароль: admin777</dd>
+</dl>
